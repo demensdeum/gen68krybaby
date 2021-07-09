42bff3679c2ec6be4a77f199cc6a9fdc498102b3
Ilia Prokhorov (Demensdeum) <demensdeum@gmail.com>
2021-07-10 00:08:31 +0300

    Labels

--- gen68krybaby.py
+++ gen68krybaby.py
@@ -6,6 +6,11 @@ if len(sys.argv) < 3:
     print("Usage python3 gen68krybaby.py genesisRom.bin output.disasm")
     exit(1)
 
+def convertLine(line):
+    output = line.replace("4E B9", "_JUMP")
+    output = output.replace("4E 71", "_NOP_")
+    return output
+
 filepath = sys.argv[1]
 rom = open(filepath, 'rb')
 content = rom.read()
@@ -13,17 +18,22 @@ rom.close()
 hexString = str(binascii.hexlify(content))[2:-1]
 disasmFilePath = sys.argv[2]
 disasm = open(disasmFilePath, 'w')
-disasm.write("0x00000000 | ")
+outputLine = "\n\t\t\t\t\t\t ROM HEADER:\n\n"
+outputLine += "0x00000000 | "
 for index, char in enumerate(hexString):
-    disasm.write(str(char).upper())
+    outputLine += str(char).upper()
     if index % 2 == 1:
-        disasm.write(" ")
+        outputLine += " "
     if index % 32 == 31 and index != len(hexString)-1:
-        disasm.write("\n")
+        outputLine += "\n"
         value = int(index / 2) + 1
+        if value == 512:
+            outputLine += "\n\t\t\t\t\t\t RESET:\n\n"
         padding = 10
-        disasm.write(f"{value:#0{padding}x}".upper())
-        disasm.write(str(" | "))
+        outputLine += f"{value:#0{padding}x}".upper()
+        outputLine += str(" | ")
+        disasm.write(convertLine(outputLine))
+        outputLine = ""
         
 disasm.close()
 
