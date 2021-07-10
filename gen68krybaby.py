b03c321c850401fbba93ccc2f63ec680da706f2a
Ilia Prokhorov (Demensdeum) <demensdeum@gmail.com>
2021-07-10 14:43:48 +0300

    Disasm subroutines

--- gen68krybaby.py
+++ gen68krybaby.py
@@ -22,7 +22,7 @@ class ChunkReader:
         
     def nextChunk(self):
         self.cursor += 1
-        address = self.cursor / 2
+        address = int(self.cursor / 2)
         if self.cursor >= len(self.hexString):
             return None
         lhs = self.hexString[self.cursor]
@@ -37,7 +37,7 @@ class DisassemblerToChunkReaderAdapter:
     def __init__(self, chunkReader):
         self.chunkReader = chunkReader
 
-    def fetchMemoryAddress(self):
+    def fetchLongWordMemoryAddress(self):
         output = "0x"
         for i in range(4):
             output += self.chunkReader.nextChunk().data
@@ -49,14 +49,24 @@ class Disassembler:
         Data = 1
         Commands = 2            
     
-    def __init__(self, dataSource):
+    def __init__(self, input, output):
         self.EntryPoint = 512
         
+        self.subroutinesAdresses = {self.EntryPoint}
         self.segmentStartAddress = None
         self.lhsChunk = None
         self.rhsChunk = None
         self.state = self.State.Data
-        self.dataSource = dataSource
+        self.input = input
+        self.output = output
+        
+    def readableAddress(self, address):
+        padding = 10
+        output = f"{address:#0{padding}x}"
+        return output
+    
+    def addSubroutineAddress(self, address):
+        self.subroutinesAdresses.add(int(address, base=16))
         
     def disasmCommandChunks(self):
         output = f"{self.lhsChunk.data}{self.rhsChunk.data}".upper()
@@ -66,53 +76,76 @@ class Disassembler:
         
         output = output.replace("4EB9", "JSR")
         output = output.replace("4E71", "NOP")
-        output = output.replace("4E75","RTS")
+        output = output.replace("4E75", "RTS")
+        output = output.replace("4E73", "RTE")
+        output = output.replace("4E77", "RTR")
+        output = output.replace("2079", "MOVEA.L,A0")
+        output = output.replace("2279", "MOVEA.L,A1")
         
         if output == "JSR":
-            memoryAddress = self.dataSource.fetchMemoryAddress()
-            output = f"JSR {memoryAddress}\n"
+            address = self.input.fetchLongWordMemoryAddress()
+            self.addSubroutineAddress(address)
+            output = f"JSR {address}\n"
+            return output
+
+        elif output == "MOVEA.L,A0":
+            address = self.input.fetchLongWordMemoryAddress()
+            self.addSubroutineAddress(address)
+            output = f"MOVEA.L {address},A0\n"
+            return output
+
+        elif output == "MOVEA.L,A1":
+            address = self.input.fetchLongWordMemoryAddress()
+            self.addSubroutineAddress(address)
+            output = f"MOVEA.L {address},A1\n"
             return output
         
         elif output == "NOP":
             output = "NOP\n"
             return output
         
+        elif output == "RTS":
+            output = "RTS\n"
+            return output        
+        
+        elif output == "RTE":
+            output = "RTE\n"
+            return output
+        
+        elif output == "RTR":
+            output = "RTR\n"
+            return output        
+        
         return f"{output}\n"
         
     def disasm(self, chunk):
         if self.segmentStartAddress == None:
             self.segmentStartAddress = chunk.address
-            return f"ROM HEADER:\n"
+            self.output.write("ROM HEADER:\n")
         
-        elif chunk.address == self.EntryPoint:
-            self.segmentStartAddress = chunk.address
+        if chunk.address in self.subroutinesAdresses:
             self.state = self.State.Commands
-            self.lhsChunk = chunk
-            return f"\nRESET:\n"
+            self.output.write(f"\nSUBROUTINE_{self.readableAddress(chunk.address)}:\n")
         
         if self.state == self.State.Commands:
             if self.lhsChunk == None:
                 self.lhsChunk = chunk
-                return ""
-            
             elif self.rhsChunk == None:
                 self.rhsChunk = chunk
-                return self.disasmCommandChunks()
-        else:
-            return ""
+                output = self.disasmCommandChunks()
+                self.output.write(f"\t\t{output}")
 
 filePath = sys.argv[1]
 disasmFilePath = f"{filePath}.disasm"
 disasm = open(disasmFilePath, 'w')
 chunkReader = ChunkReader(filePath)
-disassemblerToChunkReaderAdapter = DisassemblerToChunkReaderAdapter(chunkReader)
-disassembler = Disassembler(disassemblerToChunkReaderAdapter)
+adapter = DisassemblerToChunkReaderAdapter(chunkReader)
+disassembler = Disassembler(adapter, disasm)
 
 while True:
     chunk = chunkReader.nextChunk()
     if chunk != None:
         disassembledChunk = disassembler.disasm(chunk)
-        disasm.write(disassembledChunk)
     else:
         break
     
